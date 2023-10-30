2ffd78de96ae0f2bb178a217f8e6fc8e611e645f
ryujinmax <santi.rra40@gmail.com>
2023-10-30 08:56:48 +0700

    Update Bug Dzikir Pagi Petang (force close)

--- app/src/main/java/com/my/dzikirapp/PagiPetangDzikirActivity.kt
+++ app/src/main/java/com/my/dzikirapp/PagiPetangDzikirActivity.kt
@@ -7,6 +7,8 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
 import com.my.dzikirapp.databinding.ActivityPagiPetangDzikirBinding
+import com.my.dzikirapp.ui.DzikirPagiActivity
+import com.my.dzikirapp.ui.DzikirPetangActivity
 
 class PagiPetangDzikirActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -15,21 +17,21 @@ class PagiPetangDzikirActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
-        _binding = ActivityPagiPetangDzikirBinding.inflate(layoutInflater)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        _binding = ActivityPagiPetangDzikirBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val btnPagi = findViewById<ImageButton>(R.id.img_btn_dzikir_pagi)
         val btnPetang = findViewById<ImageButton>(R.id.img_btn_dzikir_petang)
 
-        btnPagi.setOnClickListener {this}
-        btnPetang.setOnClickListener {this}
+        btnPagi.setOnClickListener(this)
+        btnPetang.setOnClickListener(this)
     }
 
     override fun onClick(view: View?) {
         when (view?.id) {
-            R.id.img_btn_dzikir_pagi -> startActivity(Intent(this, HarianDzikirDoa::class.java))
-            R.id.img_btn_dzikir_petang -> startActivity(Intent(this, HarianDzikirDoa::class.java))
+            R.id.img_btn_dzikir_pagi -> startActivity(Intent(this, DzikirPagiActivity::class.java))
+            R.id.img_btn_dzikir_petang -> startActivity(Intent(this, DzikirPetangActivity::class.java))
         }
     }
 
